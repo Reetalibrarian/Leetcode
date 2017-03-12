4a96bd3b5c012de8c5c6131409e8bc64e080f516
YILING <yiling@candyhouse.co>
2017-03-12 23:29:50 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Yi-Ling Wu on 12/03/2017.
+//  Copyright © 2017 YILING. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
