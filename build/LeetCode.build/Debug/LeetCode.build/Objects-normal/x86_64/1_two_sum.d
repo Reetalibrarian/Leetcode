ffd4f9b72a416899cf0ffd8b3b47a3453ab468b8
YILING <yiling@candyhouse.co>
2017-03-13 23:51:45 +0800

    [ignore]*.o/*.a/*hmap/*.d/*.dia/*/dgph

--- build/LeetCode.build/Debug/LeetCode.build/Objects-normal/x86_64/1_two_sum.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/YLibrarian/Developer/Leetcode/Algorithm_1/LeetCode/LeetCode/1_two_sum.cpp \
-  /Users/YLibrarian/Developer/Leetcode/Algorithm_1/LeetCode/LeetCode/1_two_sum.hpp
